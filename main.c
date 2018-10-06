45b204b6792742f8283b64b304ae2cc004777873
Alex Almansa <alex.almansa.2016@salleurl.edu>
2018-10-06 11:20:42 +0200

    test Added some tests to try the new features
    Added a little test to try the updates to the function inserirDavant

--- main.c
+++ main.c
@@ -5,8 +5,27 @@
 
 int main (int argc, char** argv) {
     Llista list;
-    int ok;
+    int error;
+    int i = 0;
     list = LLISTABID_crea();
 
+    error= LLISTABID_vesInici(&list);
+    error = LLISTABID_inserirDavant(&list, 5);
+    error = LLISTABID_inserirDavant(&list, 4);
+    error = LLISTABID_inserirDavant(&list, 6);
+
+
+    error= LLISTABID_vesInici(&list);
+
+    error = LLISTABID_consulta(list);
+    error = LLISTABID_avanca(&list);
+    error = LLISTABID_consulta(list);
+    error = LLISTABID_avanca(&list);
+    error = LLISTABID_consulta(list);
+    error = LLISTABID_avanca(&list);
+
+    printf("ELEMENTS %d\n", list.elements);
+
+    printf("%d\n", error);
 
 }
