335eeb05faf4065050a59eacda1743b7c2ecca64
Alex Almansa <alex.almansa.2016@salleurl.edu>
2018-10-06 11:44:17 +0200

    refactor Improved the way the testing is done
    Made a better testing that makes it clearer than the list is working properly

--- main.c
+++ main.c
@@ -8,24 +8,27 @@ int main (int argc, char** argv) {
     int error;
     int i = 0;
     list = LLISTABID_crea();
+    printf("Elements inicials a la llista %d\n", list.elements);
 
     error= LLISTABID_vesInici(&list);
     error = LLISTABID_inserirOrd(&list, 5);
     error = LLISTABID_inserirOrd(&list, 4);
     error = LLISTABID_inserirOrd(&list, 6);
-
+    error = LLISTABID_inserirOrd(&list, 10);
+    error = LLISTABID_inserirOrd(&list, 20);
+    error = LLISTABID_inserirOrd(&list, 1);
 
     error= LLISTABID_vesInici(&list);
 
-    error = LLISTABID_consulta(list);
-    error = LLISTABID_avanca(&list);
-    error = LLISTABID_consulta(list);
-    error = LLISTABID_avanca(&list);
-    error = LLISTABID_consulta(list);
-    error = LLISTABID_avanca(&list);
+    while (error){
+        error = LLISTABID_consulta(list);
+        error = LLISTABID_avanca(&list);
 
-    printf("ELEMENTS %d\n", list.elements);
+    }
 
-    printf("%d\n", error);
+
+    printf("Elements Finals a la llista %d\n", list.elements);
+
+    
 
 }
